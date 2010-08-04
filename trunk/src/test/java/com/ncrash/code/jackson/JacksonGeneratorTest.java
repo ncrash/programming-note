c8ebb57fc159393ecb563147845207a74dff9f5f
ncrash <ncrash@29d8afbe-39c0-11df-b79a-85257740e83b>
2010-08-04 07:58:43 +0000

    temporary directory를 이용해 test 진행하도록 개선
    git-svn-id: svn://192.168.192.237:3690/java-code-practice-with-tdd@405 29d8afbe-39c0-11df-b79a-85257740e83b

--- trunk/src/test/java/com/ncrash/code/jackson/JacksonGeneratorTest.java
+++ trunk/src/test/java/com/ncrash/code/jackson/JacksonGeneratorTest.java
@@ -1,12 +1,20 @@
 package com.ncrash.code.jackson;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.codehaus.jackson.JsonEncoding;
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * JacksonGeneratorTest.java
@@ -16,12 +24,22 @@ import org.junit.Test;
  * @see
  */
 public class JacksonGeneratorTest {
+	
+	private ObjectMapper mapper;
 
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder(); 
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
 	@Test
-	public void testStreamingAPIJsonGeneratorExample() throws IOException {
+	public void testStreamingAPIJsonGeneratorExample() throws JsonParseException, JsonMappingException, IOException {
 		JsonFactory f = new JsonFactory();
-		JsonGenerator g = f.createJsonGenerator(new File("user.json"), JsonEncoding.UTF8);
-
+		JsonGenerator g = f.createJsonGenerator(folder.newFile("user.json"), JsonEncoding.UTF8);
+		mapper = new ObjectMapper();
+		
 		g.writeStartObject();
 		g.writeObjectFieldStart("name");
 		g.writeStringField("first", "Joe");
@@ -35,5 +53,32 @@ public class JacksonGeneratorTest {
 		g.writeEndObject();
 		g.close(); // important: will force flushing of output, close underlying output stream
 
+		File userjsonFile = new File(folder.getRoot().getAbsolutePath() + "/user.json");
+		User user = mapper.readValue(userjsonFile, User.class);
+		
+		assertUser(user);
+	}
+	
+	private void assertLastname(String lastName) {
+		assertEquals("Sixpack", lastName);
+	}
+
+	private void assertFirstname(String firstName) {
+		assertEquals("Joe", firstName);
+	}
+	
+	private void assertGender(String gender) {
+		assertEquals(User.Gender.MALE.toString(), gender);
+	}
+	
+	private void assertVerified(boolean verified) {
+		assertEquals(false, verified);
+	}
+	
+	private void assertUser(User user) {
+		assertFirstname(user.getName().getFirst());
+		assertLastname(user.getName().getLast());
+		assertGender(user.getGender().toString());
+		assertVerified(user.isVerified());
 	}
 }
