80a49a5632b384d317253e40ba78af59ea9d4ff6
ncrash <ncrash@29d8afbe-39c0-11df-b79a-85257740e83b>
2012-01-09 08:20:47 +0000

    Generate random numbers - http://www.javapractices.com/topic/TopicAction.do?Id=62
    git-svn-id: svn://192.168.192.237:3690/java-code-practice-with-tdd@725 29d8afbe-39c0-11df-b79a-85257740e83b

---
+++ trunk/src/main/java/com/ncrash/code/random/RandomGaussian.java
@@ -0,0 +1,14 @@
+package com.ncrash.code.random;
+
+import java.util.Random;
+
+public class RandomGaussian {
+	public RandomGaussian() {
+	}
+	
+	private Random fRandom = new Random();
+
+	public double getGaussian(double aMean, double aVariance) {
+		return aMean + fRandom.nextGaussian() * aVariance;
+	}
+}
---
+++ trunk/src/test/java/com/ncrash/code/random/RandomIntegerTest.java
@@ -0,0 +1,62 @@
+package com.ncrash.code.random;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+/**
+ * @author Korea R&D Team Daekwon.Kang
+ *
+ * http://www.javapractices.com/topic/TopicAction.do?Id=62
+ */
+public class RandomIntegerTest {
+
+	@Test
+	public void randomInteger() throws Exception {
+		// note a single Random object is reused here
+		Random randomGenerator = new Random();
+		for (int idx = 1; idx <= 10; ++idx) {
+			int randomInt = randomGenerator.nextInt(100);
+
+			log("idx(" + idx + ") : " + randomInt);
+		}
+	}
+
+	@Test
+	public void randomRange() throws Exception {
+		int START = 10;
+		int END = 20;
+		Random random = new Random();
+		for (int idx = 1; idx <= 10; ++idx) {
+			showRandomInteger(START, END, random);
+		}
+		
+		log("Done.");
+	}
+
+	private void showRandomInteger(int aStart, int aEnd, Random aRandom) {
+		if (aStart > aEnd) {
+			throw new IllegalArgumentException("Start cannot exceed End.");
+		}
+		// get the range, casting to long to avoid overflow problems
+		long range = (long) aEnd - (long) aStart + 1;
+		// compute a fraction of the range, 0 <= frac < range
+		long fraction = (long) (range * aRandom.nextDouble());
+		int randomNumber = (int) (fraction + aStart);
+		log("Generated : " + randomNumber);
+	}
+
+	@Test
+	public void randomGaussian() throws Exception {
+		RandomGaussian gaussian = new RandomGaussian();
+		double MEAN = 100.0f;
+		double VARIANCE = 5.0f;
+		for (int idx = 1; idx <= 10; ++idx) {
+			log("Generated : " + gaussian.getGaussian(MEAN, VARIANCE));
+		}
+	}
+
+	private static void log(Object aMsg) {
+		System.out.println(String.valueOf(aMsg));
+	}
+}
