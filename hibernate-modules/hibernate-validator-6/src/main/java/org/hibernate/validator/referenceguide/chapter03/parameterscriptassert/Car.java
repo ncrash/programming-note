12ff5bc14dd6e2becbc267860489ffaad82c1cc7
Daekwon.Kang <daekwon.kang@gmail.com>
2018-07-06 16:59:07 +0900

    #3 hibernate-modules/hibernate-validator-6 생성 #14 Chapter 1장 ~ Chapter 3장 실습

---
+++ hibernate-modules/hibernate-validator-6/src/main/java/org/hibernate/validator/referenceguide/chapter03/parameterscriptassert/Car.java
@@ -0,0 +1,19 @@
+//tag::include[]
+package org.hibernate.validator.referenceguide.chapter03.parameterscriptassert;
+
+//end::include[]
+
+import java.util.List;
+
+import org.hibernate.validator.constraints.ParameterScriptAssert;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+//tag::include[]
+public class Car {
+
+	@ParameterScriptAssert(lang = "javascript", script = "luggage.size() <= passengers.size() * 2")
+	public void load(List<Person> passengers, List<PieceOfLuggage> luggage) {
+		//...
+	}
+}
+//end::include[]
