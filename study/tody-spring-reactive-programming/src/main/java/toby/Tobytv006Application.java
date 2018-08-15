01ef9554035b2fe51df2226b273bcdc3e689c1f7
Daekwon.Kang <daekwon.kang@gmail.com>
2018-08-15 23:19:54 +0900

    #29 - Operators : spring-boot-webflux dependency 설정으로 기존 한땀 한땀 dependency 설정했던 부분 생략. HelloController.java java.lang.StackOverflowError: null 이슈 있음

---
+++ study/tody-spring-reactive-programming/src/main/java/toby/Tobytv006Application.java
@@ -0,0 +1,11 @@
+package toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tobytv006Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Tobytv006Application.class);
+	}
+}
