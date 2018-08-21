a28fd396db5dcbbe823c23d49f04916c19ecac5a
Daekwon.Kang <daekwon.kang@gmail.com>
2018-08-21 21:58:47 +0900

    #26 - 기능과 책임 분리 : 분리연습 - apply fully line coverage

--- study/madvirus-oop-guide/src/test/java/com/inflearn/oop/movie_tobe/RentalTest.java
+++ study/madvirus-oop-guide/src/test/java/com/inflearn/oop/movie_tobe/RentalTest.java
@@ -6,11 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RentalTest {
 	@Test
-	public void test_신규출시_영화_대여비() {
+	public void test_신규출시_영화_3일_대여비() {
 		Movie movie = new NewReleaseMovie();
 		Rental rental = new Rental(movie, 3);
 
 		assertThat(rental.getFrequentRenterPoints()).isEqualTo(2);
+	}
+
+	@Test
+	public void test_신규출시_영화_1일_대여비() {
+		Movie movie = new NewReleaseMovie();
+		Rental rental = new Rental(movie, 1);
+
+		assertThat(rental.getFrequentRenterPoints()).isEqualTo(1);
 	}
 
 	@Test
