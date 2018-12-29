beb46dc7fe99c7bcc3fc362789fd490fc4aff062
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 11:14:54 +0900

    #35 - 03. Learn how to create Mono instances - Resolve Quiz

--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part02Mono.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part02Mono.java
@@ -12,35 +12,50 @@ import reactor.core.publisher.Mono;
  *
  * @author Sebastien Deleuze
  * @see <a href="http://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
+ * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html#or-reactor.core.publisher.Mono-">Mono.or - Javadoc</a>
  */
 public class Part02Mono {
 
 	//========================================================================================
 
-	// TODO Return an empty Mono
+	/**
+	 * Return an empty Mono
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html#empty--">Mono Javadoc</a>
+	 */
 	Mono<String> emptyMono() {
-		return null;
+		return Mono.empty();
 	}
 
 	//========================================================================================
 
-	// TODO Return a Mono that never emits any signal
+	/**
+	 * Return a Mono that never emits any signal
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html#never--">Mono Javadoc</a>
+	 */
 	Mono<String> monoWithNoSignal() {
-		return null;
+		return Mono.never();
 	}
 
 	//========================================================================================
 
-	// TODO Return a Mono that contains a "foo" value
+	/**
+	 * Return a Mono that contains a "foo" value
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html#just-T-">Mono Javadoc</a>
+	 */
 	Mono<String> fooMono() {
-		return null;
+		return Mono.just("foo");
 	}
 
 	//========================================================================================
 
-	// TODO Create a Mono that emits an IllegalStateException
+	/**
+	 * Create a Mono that emits an IllegalStateException
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html#error-java.lang.Throwable-">Mono Javadoc</a>
+	 */
 	Mono<String> errorMono() {
-		return null;
+		return Mono.error(new IllegalStateException());
 	}
-
 }
