2857ea9a4f4aa3d7bb9ad51d428a02e97654e48f
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 14:27:44 +0900

    #35 - 05. Learn to transform our asynchronous data - Resolve Quiz

--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part04Transform.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part04Transform.java
@@ -13,28 +13,40 @@ import reactor.core.publisher.Mono;
  * Learn how to transform values.
  *
  * @author Sebastien Deleuze
+ * @see <a href="https://tech.io/playgrounds/929/reactive-programming-with-reactor-3/transform">Chapter</a>
  */
 public class Part04Transform {
 
-//========================================================================================
-
-	// TODO Capitalize the user username, firstname and lastname
+	/**
+	 * Capitalize the user username, firstname and lastname
+	 *
+	 * @param mono
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html#map-java.util.function.Function-">Mono Javadoc</a>
+	 */
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(u -> new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
-//========================================================================================
-
-	// TODO Capitalize the users username, firstName and lastName
+	/**
+	 * Capitalize the users username, firstName and lastName
+	 *
+	 * @param flux
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#map-java.util.function.Function-">Flux Javadoc</a>
+	 */
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(u -> new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	/**
+	 * Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	 *
+	 * @param flux
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#flatMap-java.util.function.Function-">Flux Javadoc</a>
+	 */
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
