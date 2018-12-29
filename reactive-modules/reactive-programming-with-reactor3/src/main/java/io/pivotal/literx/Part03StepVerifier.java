edf410809f26bb2afc9f81d737f11f09f498b31b
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 12:01:08 +0900

    #35 - 04. StepVerifier and how to use it - Resolve Quiz

--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -17,10 +17,14 @@
 package io.pivotal.literx;
 
 //generic imports to help with simpler IDEs (ie tech.io)
-import java.util.function.*;
 
 import io.pivotal.literx.domain.User;
+import org.assertj.core.api.Assertions;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.function.Supplier;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -32,43 +36,49 @@ public class Part03StepVerifier {
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
+	// Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
 	void expectFooBarComplete(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+			.expectNext("foo")
+			.expectNext("bar")
+			.verifyComplete();
 	}
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
+	// Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+			.expectNext("foo")
+			.expectNext("bar")
+			.verifyError(RuntimeException.class);
 	}
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
+	// Use StepVerifier to check that the flux parameter emits a User with "swhite"username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		StepVerifier.create(flux)
+			.assertNext(user -> Assertions.assertThat(user.getUsername()).isEqualTo("swhite"))
+			.assertNext(user -> Assertions.assertThat(user.getUsername()).isEqualTo("jpinkman"))
+			.verifyComplete();
 	}
 
 //========================================================================================
 
-	// TODO Expect 10 elements then complete and notice how long the test takes.
+	// Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier.create(flux)
+			.expectNextCount(10)
+			.verifyComplete();
 	}
 
 //========================================================================================
 
-	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
+	// Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
 	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
+		StepVerifier.withVirtualTime(supplier).thenAwait(Duration.ofHours(1)).expectNextCount(3600).verifyComplete();
 	}
-
-	private void fail() {
-		throw new AssertionError("workshop not implemented");
-	}
-
 }
--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/domain/User.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/domain/User.java
@@ -1,4 +1,13 @@
 package io.pivotal.literx.domain;
 
 public class User {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
