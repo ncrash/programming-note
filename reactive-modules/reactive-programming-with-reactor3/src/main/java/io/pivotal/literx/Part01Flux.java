bc7791180b8541379fc58f3f7040184bc8469da7
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 11:02:31 +0900

    #35 - 02. Learn how to create Flux instances - link to javadoc

--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part01Flux.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part01Flux.java
@@ -24,28 +24,44 @@ public class Part01Flux {
 
 	//========================================================================================
 
-	// Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+	/**
+	 * Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#just-T...-">Flux Javadoc</a>
+	 */
 	Flux<String> fooBarFluxFromValues() {
 		return Flux.just("foo", "bar");
 	}
 
 	//========================================================================================
 
-	// Create a Flux from a List that contains 2 values "foo" and "bar"
+	/**
+	 * Create a Flux from a List that contains 2 values "foo" and "bar"
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#fromIterable-java.lang.Iterable-">Flux Javadoc</a>
+	 */
 	Flux<String> fooBarFluxFromList() {
 		return Flux.fromIterable(Arrays.asList("foo", "bar"));
 	}
 
 	//========================================================================================
 
-	// TODO Create a Flux that emits an IllegalStateException
+	/**
+	 * Create a Flux that emits an IllegalStateException
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#error-java.util.function.Supplier-">Flux Javadoc</a>
+	 */
 	Flux<String> errorFlux() {
 		return Flux.error(new IllegalStateException());
 	}
 
 	//========================================================================================
 
-	// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
+	/**
+	 * Create a Flux that emits increasing values from 0 to 9 each 100ms
+	 *
+	 * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#interval-java.time.Duration-">Flux Javadoc</a>
+	 */
 	Flux<Long> counter() {
 		return Flux.interval(Duration.ofMillis(100)).take(10);
 	}
