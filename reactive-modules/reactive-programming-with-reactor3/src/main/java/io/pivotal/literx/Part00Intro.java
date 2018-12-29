4ee1293a055fbc479f4ae97932e76962113893c8
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 10:38:45 +0900

    #35 - rearrange package structure

--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part00Intro.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part00Intro.java
@@ -1,8 +1,8 @@
-package chapter01;
+package io.pivotal.literx;
 
 import reactor.core.publisher.Flux;
 
-public class Quiz04 {
+public class Part00Intro {
 	public static void main(String[] args) {
 		final Flux<String> flux = Flux.just("A");
 		final Flux<String> flux1 = flux.map(s -> "foo" + s);
