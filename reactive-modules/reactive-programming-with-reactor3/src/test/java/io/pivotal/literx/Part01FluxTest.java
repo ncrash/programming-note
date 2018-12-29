670942fb771c656554ba2e2275866686dc3fa7f4
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 14:41:10 +0900

    #35 - improve readability variable name

--- reactive-modules/reactive-programming-with-reactor3/src/test/java/io/pivotal/literx/Part01FluxTest.java
+++ reactive-modules/reactive-programming-with-reactor3/src/test/java/io/pivotal/literx/Part01FluxTest.java
@@ -4,36 +4,36 @@ import org.junit.Test;
 import reactor.test.StepVerifier;
 
 public class Part01FluxTest {
-	private Part01Flux part01Flux = new Part01Flux();
+	private Part01Flux workshop = new Part01Flux();
 
 	@Test
 	public void empty() {
-		StepVerifier.create(part01Flux.emptyFlux()).verifyComplete();
+		StepVerifier.create(workshop.emptyFlux()).verifyComplete();
 	}
 
 	@Test
 	public void fromValues() {
-		StepVerifier.create(part01Flux.fooBarFluxFromValues())
+		StepVerifier.create(workshop.fooBarFluxFromValues())
 			.expectNext("foo", "bar")
 			.verifyComplete();
 	}
 
 	@Test
 	public void fromList() {
-		StepVerifier.create(part01Flux.fooBarFluxFromList())
+		StepVerifier.create(workshop.fooBarFluxFromList())
 			.expectNext("foo", "bar")
 			.verifyComplete();
 	}
 
 	@Test
 	public void error() {
-		StepVerifier.create(part01Flux.errorFlux())
+		StepVerifier.create(workshop.errorFlux())
 			.verifyError(IllegalStateException.class);
 	}
 
 	@Test
 	public void countEach100ms() {
-		StepVerifier.create(part01Flux.counter())
+		StepVerifier.create(workshop.counter())
 			.expectNext(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
 			.verifyComplete();
 	}
--- reactive-modules/reactive-programming-with-reactor3/src/test/java/io/pivotal/literx/Part02MonoTest.java
+++ reactive-modules/reactive-programming-with-reactor3/src/test/java/io/pivotal/literx/Part02MonoTest.java
@@ -6,17 +6,17 @@ import reactor.test.StepVerifier;
 import java.time.Duration;
 
 public class Part02MonoTest {
-	final Part02Mono part02Mono = new Part02Mono();
+	private Part02Mono workshop = new Part02Mono();
 
 	@Test
 	public void empty() {
-		StepVerifier.create(part02Mono.emptyMono())
+		StepVerifier.create(workshop.emptyMono())
 			.verifyComplete();
 	}
 
 	@Test
 	public void noSignal() {
-		StepVerifier.create(part02Mono.monoWithNoSignal())
+		StepVerifier.create(workshop.monoWithNoSignal())
 			.expectSubscription()
 			.expectNoEvent(Duration.ofSeconds(1))
 			.thenCancel()
@@ -25,14 +25,14 @@ public class Part02MonoTest {
 
 	@Test
 	public void fromValue() {
-		StepVerifier.create(part02Mono.fooMono())
+		StepVerifier.create(workshop.fooMono())
 			.expectNext("foo")
 			.verifyComplete();
 	}
 
 	@Test
 	public void error() {
-		StepVerifier.create(part02Mono.errorMono())
+		StepVerifier.create(workshop.errorMono())
 			.verifyError(IllegalStateException.class);
 	}
 }
