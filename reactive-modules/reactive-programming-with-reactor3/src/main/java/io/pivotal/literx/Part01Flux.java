9c207e7a5dcd1068aafd2051f8a88cf2de0251ca
Daekwon.Kang <daekwon.kang@gmail.com>
2018-12-29 10:39:01 +0900

    #35 - 02. Learn how to create Flux instances - Resolve Quiz

--- reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part01Flux.java
+++ reactive-modules/reactive-programming-with-reactor3/src/main/java/io/pivotal/literx/Part01Flux.java
@@ -1,4 +1,4 @@
-package chapter02;
+package io.pivotal.literx;
 
 //generic imports to help with simpler IDEs (ie tech.io)
 import java.util.*;
@@ -17,37 +17,37 @@ public class Part01Flux {
 
 	//========================================================================================
 
-	// TODO Return an empty Flux
+	// Return an empty Flux
 	Flux<String> emptyFlux() {
-		return null;
+		return Flux.empty();
 	}
 
 	//========================================================================================
 
-	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
+	// Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+		return Flux.just("foo", "bar");
 	}
 
 	//========================================================================================
 
-	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
+	// Create a Flux from a List that contains 2 values "foo" and "bar"
 	Flux<String> fooBarFluxFromList() {
-		return null;
+		return Flux.fromIterable(Arrays.asList("foo", "bar"));
 	}
 
 	//========================================================================================
 
 	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.error(new IllegalStateException());
 	}
 
 	//========================================================================================
 
 	// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
 	Flux<Long> counter() {
-		return null;
+		return Flux.interval(Duration.ofMillis(100)).take(10);
 	}
 
 }
