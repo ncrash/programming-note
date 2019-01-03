26c3206fd68b22d84745d00a2eef682355ab1c9e
Daekwon.Kang <daekwon.kang@gmail.com>
2019-01-04 00:33:04 +0900

    #31 Exception Handling in Java Streams - 첫번째 샘플

--- study/dzone-article/README.md
+++ study/dzone-article/README.md
@@ -6,3 +6,6 @@
     * 해당 일자 대표 아티클로 접속(위 링크)한 뒤 `4. Java Enums: How to Use Configurable Sorting Fields` 항목을 클릭하는 방식으로 접근  
 * 2018-10-16
   * [Decorator Design Pattern in Java](https://dzone.com/articles/decorator-design-pattern-in-java)
+* 2019-01-04
+  * [Exception Handling in Java Streams - DZone Java](https://dzone.com/articles/exception-handling-in-java-streams?edition=433197)
+  
---
+++ study/dzone-article/src/main/java/com/dzone/exception_handling_in_java_streams/Snippet.java
@@ -0,0 +1,36 @@
+package com.dzone.exception_handling_in_java_streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Snippet {
+	public static void main(String[] args) {
+		List<String> myList = new ArrayList<>();
+		myList.add("A");
+		myList.add("B");
+		myList.add("C");
+
+		myList.stream()
+			.map(item -> {
+				try {
+					return doSomething(item);
+				} catch (MyException e) {
+					throw new RuntimeException(e);
+				}
+			})
+			.forEach(System.out::println);
+	}
+
+	private static String doSomething(String item) {
+		if (item.equalsIgnoreCase("c")) {
+			throw new MyException();
+		} else {
+			return item;
+		}
+	}
+
+	private static class MyException extends RuntimeException {
+	}
+}
