17b7eaf7ba73355af36cd805d1cbbc18dfd7fbbf
Daekwon.Kang <daekwon.kang@gmail.com>
2019-01-13 23:07:26 +0900

    #35 version up kotlin recently version - spring-coroutines-demo

--- reactive-modules/spring-coroutines-demo/src/main/kotlin/com/example/CoroutineController.kt
+++ reactive-modules/spring-coroutines-demo/src/main/kotlin/com/example/CoroutineController.kt
@@ -3,9 +3,10 @@ package com.example
 import com.example.persistence.ReactiveAuditRepository
 import com.example.persistence.ReactiveMessageRepository
 import com.example.persistence.ReactivePeopleRepository
-import kotlinx.coroutines.experimental.Unconfined
-import kotlinx.coroutines.experimental.reactive.awaitSingle
-import kotlinx.coroutines.experimental.reactor.mono
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.reactor.mono
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -17,7 +18,7 @@ class CoroutineController(
         val auditRepository: ReactiveAuditRepository) {
 
     @GetMapping("/coroutine/{personId}")
-    fun getMessages(@PathVariable personId: String) = mono(Unconfined) {
+    fun getMessages(@PathVariable personId: String) = GlobalScope.mono(Dispatchers.Unconfined) {
         val person = peopleRepository.findById(personId).awaitSingle()
         val lastLogin = auditRepository.findByEmail(person.email).awaitSingle().eventDate
         val numberOfMessages = messageRepository.countByMessageDateGreaterThanAndEmail(lastLogin, person.email).awaitSingle()
