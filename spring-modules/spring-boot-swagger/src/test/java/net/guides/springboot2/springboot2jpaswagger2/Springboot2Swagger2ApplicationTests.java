799f1cb55db3d61c1abab22be377c5fcae1ddef2
Daekwon.Kang <daekwon.kang@gmail.com>
2019-01-20 23:06:10 +0900

    #34 fix test fail, change logging level

--- spring-modules/spring-boot-swagger/src/test/java/net/guides/springboot2/springboot2jpaswagger2/Springboot2Swagger2ApplicationTests.java
+++ spring-modules/spring-boot-swagger/src/test/java/net/guides/springboot2/springboot2jpaswagger2/Springboot2Swagger2ApplicationTests.java
@@ -1,12 +1,13 @@
 package net.guides.springboot2.springboot2jpaswagger2;
 
+import net.guides.springboot2.springboot2swagger2.Springboot2Swagger2Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = { Springboot2Swagger2Application.class})
 public class Springboot2Swagger2ApplicationTests {
 
 	@Test
