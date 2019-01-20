4aa706b5255223b01533f4da6095ace0e3d28e6c
Daekwon.Kang <daekwon.kang@gmail.com>
2019-01-20 22:48:38 +0900

    #34 update author info

--- spring-modules/spring-boot-swagger/src/main/java/net/guides/springboot2/springboot2swagger2/config/Swagger2Config.java
+++ spring-modules/spring-boot-swagger/src/main/java/net/guides/springboot2/springboot2swagger2/config/Swagger2Config.java
@@ -32,7 +32,7 @@ public class Swagger2Config {
 
 		return new ApiInfoBuilder().title("Spring Boot REST API")
 				.description("Employee Management REST API")
-				.contact(new Contact("Ramesh Fadatare", "www.javaguides.net", "ramesh24fadatare@gmail.com"))
+				.contact(new Contact("Daekwon Kang ", "about.me/daekwon", "daekwon.kang@gmail.com"))
 				.license("Apache 2.0")
 				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
 				.version("1.0.0")
