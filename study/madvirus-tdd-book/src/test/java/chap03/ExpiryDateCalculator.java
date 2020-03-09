171026439d4d89dcfdba6c4a291f81f7ac5e4bf2
Daekwon Kang <daekwon.kang@gmail.com>
2020-03-09 21:26:07 +0900

    #38 chap03 - step05 리팩토링 : PayData 파라미터 클래스 추가

--- study/madvirus-tdd-book/src/test/java/chap03/ExpiryDateCalculator.java
+++ study/madvirus-tdd-book/src/test/java/chap03/ExpiryDateCalculator.java
@@ -3,7 +3,7 @@ package chap03;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+    public LocalDate calculateExpiryDate(PayData data) {
+        return data.getBillingDate().plusMonths(1);
     }
 }
