5aa2c367b7078752e4037c67b8c782858cde3230
Daekwon Kang <daekwon.kang@gmail.com>
2020-03-09 21:50:20 +0900

    #38 chap03 - step10 변수 선언으로 중복코드 제거

--- study/madvirus-tdd-book/src/test/java/chap03/ExpiryDateCalculator.java
+++ study/madvirus-tdd-book/src/test/java/chap03/ExpiryDateCalculator.java
@@ -8,11 +8,13 @@ public class ExpiryDateCalculator {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-                if (YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+                if (dayLenOfCandiMon < dayOfFirstBilling) {
+                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
                 }
-                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+                return candidateExp.withDayOfMonth(dayOfFirstBilling);
             }
         }
         return payData.getBillingDate().plusMonths(addedMonths);
