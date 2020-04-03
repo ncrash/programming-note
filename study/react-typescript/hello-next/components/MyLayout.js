e226dea768695913f4bd0b247b419819dd80f086
Daekwon Kang <daekwon.kang@gmail.com>
2020-04-03 13:23:05 +0900

    next.js - Layout as a Higher Order Component

--- study/react-typescript/hello-next/components/MyLayout.js
+++ study/react-typescript/hello-next/components/MyLayout.js
@@ -6,11 +6,12 @@ const layoutStyle = {
   border: "1px solid #DDD"
 };
 
-const Layout = props => (
-  <div style={layoutStyle}>
-    <Header />
-    {props.children}
-  </div>
-);
-
-export default Layout;
+const withLayout = Page => {
+  return () => (
+    <div style={layoutStyle}>
+      <Header />
+      <Page />
+    </div>
+  );
+};
+export default withLayout;
