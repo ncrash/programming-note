f9e58aa64b34f1a73c79c9ea6e703b410e7cab25
Daekwon Kang <daekwon.kang@gmail.com>
2020-04-03 13:27:42 +0900

    next.js - Page content as a prop

--- study/react-typescript/hello-next/components/MyLayout.js
+++ study/react-typescript/hello-next/components/MyLayout.js
@@ -6,12 +6,11 @@ const layoutStyle = {
   border: "1px solid #DDD"
 };
 
-const withLayout = Page => {
-  return () => (
-    <div style={layoutStyle}>
-      <Header />
-      <Page />
-    </div>
-  );
-};
-export default withLayout;
+const Layout = props => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.content}
+  </div>
+);
+
+export default Layout;
