bb81e193d2fbf9ff3b09385244ac9c8c5e0eb059
Daekwon Kang <daekwon.kang@gmail.com>
2020-04-03 15:23:30 +0900

    next.js - Fetching Data for Pages

--- study/react-typescript/hello-next/pages/index.js
+++ study/react-typescript/hello-next/pages/index.js
@@ -1,6 +1,21 @@
 import Layout from "../components/MyLayout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
 const PostLink = props => (
   <li>
     <Link href="/p/[id]" as={`/p/${props.id}`}>
@@ -9,15 +24,15 @@ const PostLink = props => (
   </li>
 );
 
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
-      </ul>
-    </Layout>
-  );
-}
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
