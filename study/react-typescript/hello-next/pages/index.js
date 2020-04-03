0943c96cefa0be88c6cf308915447d342437002f
Daekwon Kang <daekwon.kang@gmail.com>
2020-04-03 16:59:16 +0900

    next.js - Export into a Static HTML App

--- study/react-typescript/hello-next/pages/index.js
+++ study/react-typescript/hello-next/pages/index.js
@@ -1,48 +1,31 @@
-import { useRouter } from "next/router";
-import useSWR from "swr";
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-function fetcher(url) {
-  return fetch(url).then(r => r.json());
-}
+const Index = props => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/show/[id]" as={`/show/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
 
-export default function Index() {
-  const { query } = useRouter();
-  const { data, error } = useSWR(
-    `/api/randomQuote${query.author ? "?author=" + query.author : ""}`,
-    fetcher
-  );
-  // The following line has optional chaining, added in Next.js v9.1.5,
-  // is the same as `data && data.author`
-  const author = data?.author;
-  let quote = data?.quote;
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
 
-  if (!data) quote = "Loading...";
-  if (error) quote = "Failed to fetch the quote.";
+  console.log(`Show data fetched. Count: ${data.length}`);
 
-  return (
-    <main className="center">
-      <div className="quote">{quote}</div>
-      {author && <span className="author">- {author}</span>}
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
 
-      <style jsx>{`
-        main {
-          width: 90%;
-          max-width: 900px;
-          margin: 300px auto;
-          text-align: center;
-        }
-        .quote {
-          font-family: cursive;
-          color: #e243de;
-          font-size: 24px;
-          padding-bottom: 10px;
-        }
-        .author {
-          font-family: sans-serif;
-          color: #559834;
-          font-size: 20px;
-        }
-      `}</style>
-    </main>
-  );
-}
+export default Index;
