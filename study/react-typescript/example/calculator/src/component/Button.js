fbabf5966862064971e65a282aa7f43f310dec33
Daekwon Kang <daekwon.kang@gmail.com>
2020-04-14 17:52:52 +0900

    코드 오타로 버튼일부의 렌더링이 이상하고 동작 오류도 발생

--- study/react-typescript/example/calculator/src/component/Button.js
+++ study/react-typescript/example/calculator/src/component/Button.js
@@ -22,7 +22,7 @@ export default class Button extends React.Component {
     ];
 
     return (
-      <div className={className.join(" ".trim())}>
+      <div className={className.join(" ").trim()}>
         <button onClick={this.handleClick}>{this.props.name}</button>
       </div>
     );
